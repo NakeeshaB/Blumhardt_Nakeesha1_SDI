81cd06c3b1a373bddb440d2f54756f686efa98f9
NakeeshaB <nblumhardt@fullsail.edu>
2015-09-10 19:16:53 -0400

    Prompted User
    Prompted user for name and alerted hello.

--- GoToTrainingWeek2/js/week2.js
+++ GoToTrainingWeek2/js/week2.js
@@ -6,3 +6,9 @@
  */
 //alert("Test to see if connected.");
 
+//create an age calculator
+
+//Ask at the user for their name
+var name = prompt("Let's figure out how old you are! \nPlease type in your name.");
+// use the variable and say hello
+alert("Welcome "+name+"!");

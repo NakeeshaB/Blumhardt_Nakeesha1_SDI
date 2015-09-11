99e24838a62aca4e3d50db9b0d3345f00b9edb7f
NakeeshaB <nblumhardt@fullsail.edu>
2015-09-10 22:10:09 -0400

    Prompt,Alert,Console
    Prompted the user to enter their name.  Alerted the users name in thank
    you message.  Captured user name in console.log results.

--- Blumhardt_Nakeesha_Expressions_Assignment/js/blumhardt_nakeesha_expressions_assignment.js
+++ Blumhardt_Nakeesha_Expressions_Assignment/js/blumhardt_nakeesha_expressions_assignment.js
@@ -5,4 +5,10 @@ Expressions Assignment
 09-10-15
 */
 
-alert("Test to see if connected.");
+//alert("Test to see if connected.");
+//create an ingredients list for Peanut Butter Cookie Recipe.
+
+// What is the person's name?    
+var user = prompt("Hello friend would you help me make some Peanut Butter Cookies?\nPlease tell me your name. ");
+alert("Thank you "+user+ " for your help.");
+console.log(user);

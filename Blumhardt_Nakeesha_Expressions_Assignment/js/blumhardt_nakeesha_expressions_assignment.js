f9342b9c773c51b22312c3bf247b1bd451aec8bc
NakeeshaB <nblumhardt@fullsail.edu>
2015-09-10 22:22:19 -0400

    Console.log
    Console.log user first number entry.

--- Blumhardt_Nakeesha_Expressions_Assignment/js/blumhardt_nakeesha_expressions_assignment.js
+++ Blumhardt_Nakeesha_Expressions_Assignment/js/blumhardt_nakeesha_expressions_assignment.js
@@ -12,3 +12,10 @@ Expressions Assignment
 var user = prompt("Hello friend would you help me make some Peanut Butter Cookies?\nPlease tell me your name. ");
 alert("Thank you "+user+ " for your help.");
 console.log(user);
+
+//ask the user how many cookies do they want to make?
+
+var cookieNumber = prompt(" "+user+" how many cookies would you like to make?");
+console.log(cookieNumber);
+
+alert("Sounds great.  Let's figure out how many ingredients we will need for "+cookieNumber+" cookies.");

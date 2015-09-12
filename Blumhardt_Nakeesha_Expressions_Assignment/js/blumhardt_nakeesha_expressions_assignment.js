301056ec8a9115161c669a84e24340b2d61f2f15
NakeeshaB <nblumhardt@fullsail.edu>
2015-09-12 12:41:29 -0400

    Test
    Ran test to check formulas.  Both positive and negative numbers gave
    correct results.

--- Blumhardt_Nakeesha_Expressions_Assignment/js/blumhardt_nakeesha_expressions_assignment.js
+++ Blumhardt_Nakeesha_Expressions_Assignment/js/blumhardt_nakeesha_expressions_assignment.js
@@ -40,4 +40,7 @@ var newpayoffTime= Number(newmonthlyPayment)/Number(monthsFinance);
 console.log(newpayoffTime);
 alert("Your car could be paid off in "+Number(totalcarCost)/Number(newmonthlyPayment)+ " months if you paid the extra amount.");
 
-//ran test for Nissan Altima/first payment $350*72months=25200,added an additional $150 dollars to monthly payment. New payoff was = 50.4
+// test# 1 for Nissan Altima/first payment $350*72months=25200,added an additional $150 dollars to monthly payment. New payoff was = 50.4
+// test #2 for Honda Civic/ first payment $200.00*60months =12000, added no additional funds, total payoff still 60 months.
+// test# 3 for Porsha/first payment $950.00*72months = 68400, added $300.00 additional funds, total payoff months 54.72
+// test# 4 for Honda/first payment $450.00*60months = 27000, then subtracted - $50.00 from additional funds, payoff months 67.5.

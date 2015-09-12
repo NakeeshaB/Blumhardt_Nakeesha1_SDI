dfead0fb28d6d6ad94cec145982066f24be787f0
NakeeshaB <nblumhardt@fullsail.edu>
2015-09-12 12:31:05 -0400

    Fixed Calculations/alerted user
    found fix for division problem and alerted user of new payoff amount.

--- Blumhardt_Nakeesha_Expressions_Assignment/js/blumhardt_nakeesha_expressions_assignment.js
+++ Blumhardt_Nakeesha_Expressions_Assignment/js/blumhardt_nakeesha_expressions_assignment.js
@@ -25,6 +25,19 @@ console.log(monthlyPayment);
 
 var monthsFinance = prompt(" How many months would you like to finance your car for?");
 alert("  "+user+ ", your car will cost you $"+monthlyPayment*Number(monthsFinance)+" after it is completely paid off");
-
-var carPrice = monthlyPayment*monthsFinance
+var totalcarCost = monthlyPayment*monthsFinance;
+console.log(totalcarCost);
+var carPrice = monthlyPayment*monthsFinance;
 console.log(carPrice);
+
+var additionalFunds = prompt(" " +user+ " if you could add additional money to your monthly payment, how much would you add?");
+console.log(additionalFunds);
+
+var newmonthlyPayment = Number(monthlyPayment) +Number(additionalFunds);
+console.log(newmonthlyPayment);
+
+var newpayoffTime= Number(newmonthlyPayment)/Number(monthsFinance);
+console.log(newpayoffTime);
+alert("Your car could be paid off in "+Number(totalcarCost)/Number(newmonthlyPayment)+ " months if you paid the extra amount.");
+
+//ran test for Nissan Altima/first payment $350*72months=25200,added an additional $150 dollars to monthly payment. New payoff was = 50.4

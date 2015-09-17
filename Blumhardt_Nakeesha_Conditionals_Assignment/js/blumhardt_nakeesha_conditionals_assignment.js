050922f74c31727c68096d90d9402b97e649c781
NakeeshaB <nblumhardt@fullsail.edu>
2015-09-17 18:33:59 -0400

    Prompt/Alert
    Prompt for user name and alerted them that we are going to buy a home.

--- Blumhardt_Nakeesha_Conditionals_Assignment/js/blumhardt_nakeesha_conditionals_assignment.js
+++ Blumhardt_Nakeesha_Conditionals_Assignment/js/blumhardt_nakeesha_conditionals_assignment.js
@@ -5,4 +5,11 @@
  09-17-15
  */
 
-alert("test to see if connected");
+//alert("test to see if connected");
+// Prompt the user that we are going to buy a home and ask for their name.
+
+var name = prompt("Let's buy a home.  Please tell me your name first\nthen we can figure out what type of home you can buy.");
+alert("Okay " +name+ ", let's get started.");
+
+console.log(name);
+

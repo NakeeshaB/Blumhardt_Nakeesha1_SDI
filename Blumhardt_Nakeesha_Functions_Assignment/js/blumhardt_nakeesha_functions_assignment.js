39237650baa62675e86e717c35cb25c29d893ba3
NakeeshaB <nblumhardt@fullsail.edu>
2015-09-25 11:05:32 -0400

    prompted .function, console.log
    Prompted the user to enter in measurements for the area of Box A.  Ran
    a regular function test for box “A” and console.log results.  The users
    information and function results produced the same answer.

--- Blumhardt_Nakeesha_Functions_Assignment/js/blumhardt_nakeesha_functions_assignment.js
+++ Blumhardt_Nakeesha_Functions_Assignment/js/blumhardt_nakeesha_functions_assignment.js
@@ -19,3 +19,65 @@ alert("There are 75 A boxes, 235 B boxes and 190 C boxes.");
 alert ("Let's figure out the area of 1 box of 'A' cookies that has a lenght of 7 inches and a width of 4 inches ");
 
 
+
+
+var box1 = boxA(4,7);
+
+    function boxA(w,l){
+        var area = w * l;
+        return area;
+}
+
+console.log(" The area of Cookie A is  " +box1+ " .");
+
+alert(" "  +name+ " ,you will need to use a formula that will help you find the area of a rectangle, such as Width * length.  ");
+
+var box1Width = prompt(" Please enter the width first of box A");
+
+var box1Lenght = prompt("Please enter the length of box A");
+
+alert("Now multiply the two numbers together.");
+var resultA = prompt("Please enter in your results.");
+
+var resultA =box1Width*box1Lenght;
+
+alert("The area should be  " + resultA+ " .");
+
+console.log(resultA);
+
+// regular function // regular functions can be called before or after they are defined.
+//var total = calArea(4,7);
+
+//function calArea(w ,h){
+    //var area = w * h;
+    //return area;
+//}
+
+//console.log(total);
+
+
+// anonymous function
+
+//var calArea = function(20,30){
+// var area = w * l;
+//return area;
+//}
+
+//var a = calArea(20,30); // anonymous functions are called after they are defined.
+//console.log(a);
+
+
+//function calArea(){
+  //  var width = 4;
+    //var length = 7;
+    //var area = width * length;
+    //console.log(area);
+
+//}
+//calArea();
+//var area = areaboxA(4*7);
+//function areaboxA (w,l){
+   // var area = w*l;
+    //return area;
+//}
+//console.log("area");

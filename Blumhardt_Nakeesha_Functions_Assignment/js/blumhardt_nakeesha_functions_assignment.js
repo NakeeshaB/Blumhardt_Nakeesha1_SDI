efc73fe466dadb862b74f8ec8bc24d7e067733a2
NakeeshaB <nblumhardt@fullsail.edu>
2015-09-25 21:39:31 -0400

    Prompted user
    prompted user to enter area of all boxes and decide which truck would
    fit each selection of items.

--- Blumhardt_Nakeesha_Functions_Assignment/js/blumhardt_nakeesha_functions_assignment.js
+++ Blumhardt_Nakeesha_Functions_Assignment/js/blumhardt_nakeesha_functions_assignment.js
@@ -12,9 +12,9 @@ alert(" Nice to meet you " +name+" .\n I have 500 boxes of cookies that I need t
 
 console.log(name);
 
-alert("There are 3 different boxes of cookies. Boxes A, B, and C.");
+alert("There are 3 different boxes of cookies. Boxes A(Lemon), B( Mint), and C(Smores).");
 
-alert("There are 75 A boxes, 235 B boxes and 190 C boxes.");
+alert("There are 75 Lemon boxes, 235 Mint boxes and 190 Smore boxes.");
 
 alert ("Let's figure out the area of 1 box of 'A' cookies that has a lenght of 7 inches and a width of 4 inches ");
 
@@ -45,6 +45,7 @@ var box3 = function()
     return area
 }
 
+
 var area = box3(3,7);
 console.log(" The area of Cookie C is " +area+ " inches squared .");
 
@@ -64,7 +65,7 @@ var resultA = prompt("Please enter in your results.");
 
 var resultA =box1Width*box1Lenght;
 
-alert("The area should be  " + resultA+ " inches squared .");
+alert("The area should be  " + resultA+ " squared inches .");
 
 console.log(resultA);
 
@@ -76,9 +77,9 @@ var box2Result = prompt("Please enter in your results.");
 
 var box2Result = box2Width * box2length;
 
-console.log(" The area of Cookie B is " +box2Result+ " inches squared.");
+console.log(" The area of Cookie B is " +box2Result+ "  square inches .");
 
-alert(" The area for Cookie B should be " +box2Result+ " inches squared.");
+alert(" The area for Cookie B should be " +box2Result+ " square inches .");
 
 alert(" Now lets calculate the area for Cookie C.");
 alert ("Cookie C's box has a width of 3 inches and a length of 7 inches.  Calculate the area.");
@@ -89,8 +90,34 @@ var box3Results = prompt("Enter the total area of Cookie C");
 
 var box3Results = box3Width * box3Length;
 console.log(" The area of Cookie B is "+ box3Results+ " .");
-alert(" The area of Cookie B is "+box3Results+ " inches squared.");
+alert(" The area of Cookie B is "+box3Results+ " square inches.");
 
+alert (" Now that we have area for all of the boxes let's figure out what truck each type of cookie can fit into.");
+
+alert (" Truck A can hold a maximum of 2300 square inches.  Truck B can hold a maxium of 5000 square inches and Truck C can hold 10,100 square inches. ");
+
+alert("To figure out what cookies can go in each truck we need to multiply the the square inched by the number of boxes.");
+
+var lemon= prompt(" Enter the square inch total for the lemon cookies.");
+console.log(lemon);
+
+var mint= prompt(" Enter the square inch total for the Mint cookies.");
+Console.log(mint);
+
+var smores= prompt("Enter the square inch total for the Smores cookie.");
+console.log(smores);
+
+lemon=2100;
+mint=9987.5;
+smores=3990;
+var truckA = 2300;
+var truckB = 5000;
+var truckC = 10100;
+
+
+
+
+//
 //var box1 = boxA(4,7);
 
 //function boxA(w,l){
